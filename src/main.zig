6908eac5437020c99bc61850f059ab8e15d7dc2a
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2025-02-21 16:26:23 +0100

    ball flying around

--- src/main.zig
+++ src/main.zig
@@ -10,15 +10,40 @@ const paddleHeight = 32;
 const paddlePadding = 64;
 const paddleSpeed = 300;
 
+const ballRadius = 16;
+const ballSpeed = 350;
+
 const GameState = struct {
+    paused: bool,
+
     playerPosition: f32, // paddle x
 
+    ballPosition: rl.Vector2,
+    ballDirection: rl.Vector2,
+
     fn init() GameState {
-        return .{ .playerPosition = (windowWidth - paddleWidth) / 2 };
+        return .{
+            .paused = true,
+            .playerPosition = (windowWidth - paddleWidth) / 2,
+            .ballPosition = rl.Vector2.init(windowWidth / 2, windowHeight / 2),
+            .ballDirection = rl.Vector2.zero(),
+        };
     }
 
     fn update(self: *GameState) void {
         const dt = rl.getFrameTime();
+
+        if (self.paused) {
+            self.ballPosition.x = @floatCast(windowWidth * 0.7 * std.math.sin(rl.getTime() * 0.5));
+
+            if (rl.isKeyPressed(rl.KeyboardKey.space)) {
+                const mousePos = rl.getMousePosition();
+                self.ballDirection = rl.Vector2.init(mousePos.x - self.ballPosition.x, mousePos.y - self.ballPosition.y).normalize();
+                self.paused = false;
+            }
+
+            return;
+        }
 
         if (rl.isKeyDown(rl.KeyboardKey.a)) {
             self.playerPosition -= paddleSpeed * dt;
@@ -29,10 +54,26 @@ const GameState = struct {
         }
 
         self.playerPosition = std.math.clamp(self.playerPosition, 0, windowWidth - paddleWidth);
+
+        self.ballPosition.x += self.ballDirection.x * ballSpeed * dt;
+        self.ballPosition.y += self.ballDirection.y * ballSpeed * dt;
+
+        if ((self.ballPosition.x < 0) or (self.ballPosition.x >= windowWidth - ballRadius)) {
+            self.ballDirection.x *= -1;
+        }
+
+        if (self.ballPosition.x >= self.playerPosition and self.ballPosition.x <= self.playerPosition + paddleWidth and self.ballPosition.y + ballRadius >= windowHeight - paddlePadding - paddleHeight and self.ballPosition.y + ballRadius <= windowHeight - paddlePadding) {
+            self.ballDirection.y *= -1;
+        }
+
+        if (self.ballPosition.y <= ballRadius) {
+            self.ballDirection.y *= -1;
+        }
     }
 
     fn draw(self: *GameState) void {
-        rl.drawRectangle(@intFromFloat(self.playerPosition), windowHeight - paddlePadding - paddleHeight, paddleWidth, paddleHeight, rl.Color.red);
+        rl.drawCircleV(self.ballPosition, ballRadius, rl.Color.red);
+        rl.drawRectangle(@intFromFloat(self.playerPosition), windowHeight - paddlePadding - paddleHeight, paddleWidth, paddleHeight, rl.Color.green);
     }
 };
 
